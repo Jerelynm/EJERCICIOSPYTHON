500af74e60bd2bb964aa57b10830a61c6ebb7630
Jerelynm <jmarinm4@miumg.edu.gt>
2025-10-18 09:10:06 -0600

    for10

---
+++ for10.py
@@ -0,0 +1,11 @@
+
+primos = []
+num = 2
+while len(primos) < 10:
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            break
+    else:
+        primos.append(num)
+    num += 1
+print("Primeros 10 primos:", primos)
